9d821e66da2f5ac81797faf7ee3706c21f318a2f
marcoscabral <marcoscabral90@gmail.com>
2017-11-10 18:02:15 -0300

    Comentar algo

--- tests.py
+++ tests.py
@@ -9,14 +9,14 @@ class BasicTestsCase(unittest.TestCase):
 
         self.assertTrue(10.5 > 10)
         self.assertTrue(11 > 10.5)
-        self.assertFalse(11 > 10)
+        self.assertTrue(11 > 10)
         self.assertEqual(11, 10 + 1)
-        self.assertEqual(12, 10 + 1)
+        self.assertEqual(12, 10 + 2)
 
     def test_2(self):
         self.assertIn(1, [1, 2, 3])
-        self.assertIn(2, [1, 1, 1])
-        self.assertNotIn(2, [2, 2, 2])
+        self.assertIn(2, [2, 1, 1])
+        self.assertNotIn(2, [3, 4, 5])
 
 
 class LinuxCommandsTestsCase(unittest.TestCase):
@@ -26,7 +26,7 @@ class LinuxCommandsTestsCase(unittest.TestCase):
 
         self.assertIn('ls', commands)
         self.assertIn('cd', commands)
-        self.assertIn('epp', commands)
+        self.assertIn('mkdir', commands)
 
     def _num_to_perm(self, num):
         # Don't touch this method
@@ -37,16 +37,16 @@ class LinuxCommandsTestsCase(unittest.TestCase):
         c = self._num_to_perm('777')
         self.assertEqual(c, 'rwxrwxrwx')
         c = self._num_to_perm('775')
-        self.assertEqual(c, 'rwxrwxrwx')
+        self.assertEqual(c, 'rwxrwxr-x')
         c = self._num_to_perm('644')
-        self.assertEqual(c, 'rwxrwxrwx')
+        self.assertEqual(c, 'rw-r--r--')
 
     def test_3(self):
-        c = self._num_to_perm('___')
+        c = self._num_to_perm('400')
         self.assertEqual(c, 'r--------')
-        c = self._num_to_perm('___')
+        c = self._num_to_perm('555')
         self.assertEqual(c, 'r-xr-xr-x')
-        c = self._num_to_perm('___')
+        c = self._num_to_perm('744')
         self.assertEqual(c, 'rwxr--r--')
 
 unittest.main()
